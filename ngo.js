739b669fe6bc6bfd50467885d65d95fb3c5ccb72
mastermindankur <mastermindankur@gmail.com>
2019-06-30 14:01:41 +0800

    Add files via upload

--- ngo.js
+++ ngo.js
@@ -19,12 +19,13 @@ const shim = require('fabric-shim');
 const util = require('util');
 
 var fs = require('fs');
-var log_file = fs.createWriteStream('debug.log', {flags : 'w'});
+var util1 = require('util');
+var log_file = fs.createWriteStream(__dirname + '/aish.log', {flags : 'w'});
 var log_stdout = process.stdout;
 
-const console1 = function(d) { //
-  log_file.write(util.format(d) + '\n');
-  //log_stdout.write(util.format(d) + '\n');
+console.log = function(d) { //
+  log_file.write(util1.format(d) + '\n');
+  log_stdout.write(util1.format(d) + '\n');
 };
 
 /************************************************************************************************
